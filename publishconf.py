f39ce3591148096c660e4b8efe3e02bfbde9d47b
Nhan <nhnguyen@ualberta.ca>
2021-02-08 16:42:17 -0700

    manually created docs

--- publishconf.py
+++ publishconf.py
@@ -10,15 +10,17 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
-RELATIVE_URLS = False
+SITEURL = 'https://nhtnhan.github.io/cmput404-lab5/'
 OUTPUT_PATH = "docs/"
+RELATIVE_URLS = True
+OUTPUT_SOURCES = True
+
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 
-DELETE_OUTPUT_DIRECTORY = True
+# DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
